fd5c9d06673501a2be4a457220aca8d1d2c7a05d
Pahul2004 <sharmapahul996@gmail.com>
2024-12-08 11:15:38 +0530

    Day34

--- day34.java
+++ day34.java
@@ -42,7 +42,7 @@ class Solution {
     public String convert(String s, int numRows) {
         if(numRows==1 || s.length()<=numRows)return s;
 
-        StringBuilder[]rows=new StringBuilder[numsRows];
+        StringBuilder[]rows=new StringBuilder[numRows];
         for(int i=0;i<numRows;i++)
         {
             rows[i]=new StringBuilder();
